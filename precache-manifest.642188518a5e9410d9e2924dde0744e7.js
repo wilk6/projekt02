3a9dc48ffc6142b206fa672a2172f0064f2acada
wilk6 <wilk6996@gmail.com>
2020-04-29 10:22:20 +0200

    Updates

---
+++ precache-manifest.642188518a5e9410d9e2924dde0744e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2b40fe430fd1cdf92987141c5d6765",
+    "url": "/projekt02/index.html"
+  },
+  {
+    "revision": "23795701af294d32067f",
+    "url": "/projekt02/static/css/main.0e0962ec.chunk.css"
+  },
+  {
+    "revision": "417f2021f1370d7023f8",
+    "url": "/projekt02/static/js/2.18d06d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projekt02/static/js/2.18d06d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23795701af294d32067f",
+    "url": "/projekt02/static/js/main.e8ec7311.chunk.js"
+  },
+  {
+    "revision": "ef84ff7ae54ade3d5041",
+    "url": "/projekt02/static/js/runtime-main.40f31b0c.js"
+  },
+  {
+    "revision": "23c0a1813e235baf168361e30c67fc29",
+    "url": "/projekt02/static/media/questionMark.23c0a181.jpg"
+  }
+]);
